e518027c9e3ff3505a4359bcc471496ebea12365
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-14 04:35:51 +0900

    ha ha

--- main.js
+++ main.js
@@ -2,9 +2,14 @@
 
 var w = window.innerWidth;
 var h = window.innerHeight;
+var cover = document.getElementsByClassName('cover')[0];
 
-console.log(w, h);
+console.log(w, h, cover);
 
 if(w <= 500){
-    alert('THIS WEBSITE IS NOT MOBILE RESPONSIVE!')
+    alert('THIS WEBSITE IS NOT MOBILE RESPONSIVE!');
+    cover.style.display = 'block';
+
+}else{
+    cover.style.display = 'none';
 }
